1499828ac77d81058d9b48930dbc0d8d3a91a8f9
cmd-source <conorcrosbie09@gmail.com>
2021-08-15 16:57:17 +0000

    adding in the carosel autoplay for the homepage and formatting to the other pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-    $('.carousel').carousel();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+  $('.carousel').carousel({
+    padding: 200
   });
+  autoplay();
+
+  //This fucntion was taken from https://stackoverflow.com/questions/36581504/materialize-carousel-slider-autoplay 
+  function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4000);
+  }
+});
